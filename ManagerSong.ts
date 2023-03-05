a4fdcd98dd4a4ed57d2bd5e42510db55288238df
Your Name <you@example.com>
2023-03-05 18:18:12 +0700

    Xong tim kiem tuong doi

--- ManagerSong.ts
+++ ManagerSong.ts
@@ -2,12 +2,13 @@ import {Song} from "./Song";
 
 export class ManagerSong {
     listSong: Song[] = [];
-    count: number = 0;
+    countSong: number = 1;
 
     constructor() {
     }
-    getId(){
-        return this.count++;
+
+    getIdSong() {
+        return this.countSong++;
     }
 
     addSong(song: Song): void {
@@ -18,18 +19,26 @@ export class ManagerSong {
         console.table(this.listSong);
     }
 
-    searchSongByName(nameSong: string) {
-        this.listSong.filter((value, index, array) => {
-            if (value.nameOfSong === nameSong) {
-                console.table(array[index]);
-            } else if (index === array.length-1) {
-                console.log("Khong tim thay bai hat")
-            }
-        })
+    searchSongByName(nameSong: string): void {
+        let arrSong: Song[] = this.listSong;
+        let result: Song[] = arrSong.filter(element => element.nameOfSong.includes(nameSong));
+        if (result === undefined) {
+            console.log('Eror');
+        } else {
+            console.table(result);
+        }
     }
 
-    findIndexById(id:number):number {
-        let i:number = 0;
+    // this.listSong.filter((value, index, array) => {
+    //     if (value.nameOfSong === nameSong) {
+    //         console.table(array[index]);
+    //     } else if (value.nameOfSong !== nameSong) {
+    //         console.log("Khong tim thay bai hat");
+    //     }
+    // })
+
+    findIndexById(id: number): number {
+        let i: number = 0;
         this.listSong.forEach((value, index, array) => {
             if (value.idSong === id) {
                 i = index;
@@ -38,26 +47,27 @@ export class ManagerSong {
         return i;
     }
 
-    deleteSongById(id: number):void {
-        let idOfDelete:number = this.findIndexById(id)
-        if (idOfDelete!=-1) {
+    deleteSongById(id: number): void {
+        let idOfDelete: number = this.findIndexById(id)
+        if (idOfDelete != -1) {
             this.listSong.splice(idOfDelete, 1);
         } else {
             console.log("Khong tim thay bai hat")
         }
     }
 
-    updateNameSongById(id: number, nameSong:string):void {
-        let idOfUpdate:number = this.findIndexById(id)
-        if (idOfUpdate!=-1) {
+    updateNameSongById(id: number, nameSong: string): void {
+        let idOfUpdate: number = this.findIndexById(id)
+        if (idOfUpdate != -1) {
             this.listSong[idOfUpdate].setNameOfSong(nameSong);
         } else {
             console.log("Khong tim thay bai hat")
         }
     }
-    updateNameSingerById(id: number, nameSinger:string):void {
-        let idOfUpdate:number = this.findIndexById(id)
-        if (idOfUpdate!=-1) {
+
+    updateNameSingerById(id: number, nameSinger: string): void {
+        let idOfUpdate: number = this.findIndexById(id)
+        if (idOfUpdate != -1) {
             this.listSong[idOfUpdate].setNameOfSinger(nameSinger);
         } else {
             console.log("Khong tim thay bai hat")
